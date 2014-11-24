a1ea0e73fb0f2b636407c60f2e6133d76b68a45d
Fran Martinez <martinezcoder@gmail.com>
2014-11-24 12:28:36 +0100

    improvements

--- open_closed_4.rb
+++ open_closed_4.rb
@@ -2,6 +2,10 @@ class Shape
   # ovewritte this method for the class that defines the area of your shape
   attr_writer :area
   attr_reader :area
+
+  def area
+    raise "this object doesn't have an area method"
+  end
 end
 
 class Rectangle < Shape
@@ -32,6 +36,19 @@ class Circle < Shape
   end
 end
 
+class Triangle < Shape
+  attr_accessor :base
+  attr_accessor :height
+
+  def initialize(params)
+    @base = params[:base]
+    @height = params[:height]
+  end
+
+  def area
+    base * height / 2
+  end
+end
 
 class AreaCalculator
 
@@ -50,15 +67,18 @@ end
 r1 = Rectangle.new(:width => 10, :height => 20)
 r2 = Rectangle.new(:width => 5, :height => 14)
 c1 = Circle.new(30)
+t1 = Triangle.new(:base => 10, :height => 30)
 
 puts "area r1= #{r1.area}"
 puts "area r2= #{r2.area}"
 puts "area c1= #{c1.area}"
+puts "area t1= #{t1.area}"
 
 r_collect = []
 r_collect << r1
 r_collect << r2
 r_collect << c1
+r_collect << t1
 
 puts "collection area= #{AreaCalculator.area(r_collect)}"
 
